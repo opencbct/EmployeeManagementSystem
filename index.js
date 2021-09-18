2f14e2a6c9de0abb08e5bc186b43ac010e996072
Benjamin Kim <84877261+opencbct@users.noreply.github.com>
2021-09-18 12:31:25 -0500

    index

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
-    port: 3001,
+    port: 3306,
 
     // Your username
     user: "root",
 
     // Your password
-    password: "gajahowai",
+    password: "mola18nomA",
     database: "companyDB"
 });
 
